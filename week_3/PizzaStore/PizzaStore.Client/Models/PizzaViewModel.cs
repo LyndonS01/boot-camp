cb28f46a4dbe8523b1a04607926b789808ddb2c5
lyndon santos <santos.lyndon@gmail.com>
2020-08-03 21:06:40 -0700

    fixed order modelview

--- week_3/PizzaStore/PizzaStore.Client/Models/PizzaViewModel.cs
+++ week_3/PizzaStore/PizzaStore.Client/Models/PizzaViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 using PizzaStore.Domain.Factories;
 using PizzaStore.Domain.Models;
 
@@ -21,5 +22,28 @@ namespace PizzaStore.Client.Models
     [Range(2,5)]
     public List<ToppingModel> SelectedToppings { get; set; }
     public bool SelectedTopping { get; set; }
+
+    public PizzaViewModel()
+    {
+      Crusts = new List<CrustModel>();
+      Crusts.Add(new CrustModel{Name = "Thin"});
+      Crusts.Add(new CrustModel{Name = "Thick"});
+      Crusts.Add(new CrustModel{Name = "Stuffed"});
+
+      Sizes = new List<SizeModel>();
+      Sizes.Add(new SizeModel{Name = "Regular"});
+      Sizes.Add(new SizeModel{Name = "Large"});
+      Sizes.Add(new SizeModel{Name = "Family"});
+
+      Toppings = new List<ToppingModel>();
+      Toppings.Add(new ToppingModel{Name = "cheese"});
+      Toppings.Add(new ToppingModel{Name = "extra cheese"});
+      Toppings.Add(new ToppingModel{Name = "pepperoni"});
+      Toppings.Add(new ToppingModel{Name = "ham"});
+      Toppings.Add(new ToppingModel{Name = "pineapple"});
+      Toppings.Add(new ToppingModel{Name = "mushrooms"});
+      Toppings.Add(new ToppingModel{Name = "bell peppers"});
+      Toppings.Add(new ToppingModel{Name = "sausage"});
+    }
   }
 }
